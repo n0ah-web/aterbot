16ec47e9b59a6bc91ad360615681ec6ebe1e9fae
n0ah-web <noahjklein2014@gmail.com>
2026-02-12 19:41:19 +0100

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -21,9 +21,9 @@ const reconnect = async (): Promise<void> => {
 
 const createBot = (): void => {
 	bot = Mineflayer.createBot({
-		host: CONFIG.client.host,
-		port: +CONFIG.client.port,
-		username: CONFIG.client.username
+		host:Projekt_Insel.aternos.me
+		port: 44345
+		username: heyNoahYT2
 	} as const);
 
 
